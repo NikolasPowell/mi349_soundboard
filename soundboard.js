c26e82e90fa140b8b52f9ad5581d9892f81014d1
Nikolas Powell <npowell922@comcast.net>
2022-06-18 14:31:20 -0400

    Adding Changes

--- soundboard.js
+++ soundboard.js
@@ -1,6 +1,20 @@
 var firstbuttonel = document.getElementById("first-button")
+var secondbuttonel = document.getElementById("second-button")
+var thirdbuttonel = document.getElementById("third-button")
+
 var music = new Audio('applause_y.wav')
+var music2 = new Audio('boo.wav')
+var music3 = new Audio('cheering.wav')
 
 firstbuttonel.addEventListener('click', function () {
     music.play()
 })
+
+secondbuttonel.addEventListener('click', function () {
+    music2.play()
+})
+
+thirdbuttonel.addEventListener('click', function () {
+    music3.play()
+})
+
